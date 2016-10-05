e505ae09388ae22ef26ef971700832eb0398a54f
Matias Mariani <matiasironyard@gmail.com>
2016-10-05 15:20:51 -0400

    initial html and js setup

--- app/scripts/index.js
+++ app/scripts/index.js
@@ -1,1 +1,43 @@
-console.log("Hello World!");
+var $ = require('jquery');
+var _ = require('underscore');
+var handlebars = require('handlebars');
+var url = "https://api.etsy.com/v2/listings/active.js?api_key=cdwxq4soa7q4zuavbtynj8wx&keywords=yarn&includes=Images,Shop&sort_on=score";
+
+
+
+
+
+function logData(data) {
+        console.log(data);
+      }
+
+
+
+
+
+
+
+
+
+
+      fetchJSONP(url, logData);
+
+
+
+
+
+
+
+function fetchJSONP(url, callback) {
+    var callbackName = 'jsonp_callback_' + Math.round(100000 * Math.random());
+    var script = document.createElement('script');
+
+    window[callbackName] = function(data) {
+        delete window[callbackName];
+        document.body.removeChild(script);
+        callback(data);
+    };
+
+    script.src = url + (url.indexOf('?') >= 0 ? '&' : '?') + 'callback=' + callbackName;
+    document.body.appendChild(script);
+}
